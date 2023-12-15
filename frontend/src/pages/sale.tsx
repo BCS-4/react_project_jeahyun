d5c163c1b73d583d2460f86ab8ff505d116af511
crazydumbbell <wogus5357@gmail.com>
2023-12-15 14:22:05 +0900

    complete

--- frontend/src/pages/sale.tsx
+++ frontend/src/pages/sale.tsx
@@ -1,7 +1,68 @@
-import { FC } from "react";
+// getOnSaleNFTs의 팔리고있는 토큰 id 조회
+// tokenuri로 메타데이터 조회후 axios로 불러오기
 
-const sale: FC = () => {
-  return <div>sale</div>;
+import { FC, useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import { NftMetadata, OutletContext } from "../types";
+import axios from "axios";
+import SaleNftCard from "../components/SaleNftCard";
+
+const Sale: FC = () => {
+  const [metadataArray, setMetadataArray] = useState<NftMetadata[]>([]);
+
+  const { saleNftContract, mintNftContract } =
+    useOutletContext<OutletContext>();
+
+  const getSaleNFTs = async () => {
+    try {
+      const onSaleNFTs: bigint[] = await saleNftContract.methods
+        .getOnSaleNFTs()
+        .call();
+
+      let temp: NftMetadata[] = [];
+
+      for (let i = 0; i < onSaleNFTs.length; i++) {
+        const metadataURI: string = await mintNftContract.methods
+          // @ts-expect-error
+          .tokenURI(Number(onSaleNFTs[i]))
+          .call();
+
+        const response = await axios.get(metadataURI);
+
+        temp.push({ ...response.data, tokenId: Number(onSaleNFTs[i]) });
+      }
+
+      setMetadataArray(temp);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!saleNftContract) return;
+
+    getSaleNFTs();
+  }, [saleNftContract]);
+
+  return (
+    <div className="grow">
+      <div className="text-center py-8">
+        <h1 className="font-bold text-2xl">Sale NFTs</h1>
+      </div>
+      <ul className="p-8 grid grid-cols-2 gap-8">
+        {metadataArray?.map((v, i) => (
+          <SaleNftCard
+            key={i}
+            image={v.image}
+            name={v.name}
+            tokenId={v.tokenId!}
+            metadataArray={metadataArray}
+            setMetadataArray={setMetadataArray}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-export default sale;
+export default Sale;
